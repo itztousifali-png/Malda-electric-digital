6041a6fede03b984a30146df85ce2971f6ba7650
itztousifali-png <itztousifali@gmail.com>
2025-12-26 22:25:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-function toggleMenu(){
-  let d=document.getElementById("drawer");
-  d.style.left = (d.style.left==="0px") ? "-220px" : "0px";
+function bookService(){
+let b={name:bname.value,phone:bphone.value,msg:bmsg.value};
+let list=JSON.parse(localStorage.getItem("bookings"))||[];
+list.push(b);
+localStorage.setItem("bookings",JSON.stringify(list));
+window.open("https://wa.me/918250687140?text=New Booking Received");
 }
 
-function notify(){
-  alert("🔔 New services & updates coming soon!");
+function paymentDone(){
+window.open("https://wa.me/918250687140?text=Payment Completed");
 }
 
-function whatsappAuto(){
-  let msg =
-  "Hello Malda Electric & Digital,%0A"+
-  "I need your service.%0A"+
-  "Please contact me.";
-
-  window.open("https://wa.me/918250687140?text="+msg,"_blank");
+function addReview(){
+let r={name:rname.value,msg:rmsg.value};
+let list=JSON.parse(localStorage.getItem("reviews"))||[];
+list.push(r);
+localStorage.setItem("reviews",JSON.stringify(list));
+loadReviews();
 }
+
+function loadReviews(){
+reviewList.innerHTML=(JSON.parse(localStorage.getItem("reviews"))||[])
+.map(r=>`<p>⭐ <b>${r.name}</b>: ${r.msg}</p>`).join("");
+}
+loadReviews();
