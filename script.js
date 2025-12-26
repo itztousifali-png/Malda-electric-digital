7674d49a430523005431f70c326e9ac6421b292f
itztousifali-png <itztousifali@gmail.com>
2025-12-26 21:28:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-function notify() {
-  alert("🔔 Notification Enabled!\nYou will get updates soon.");
+function toggleMenu(){
+  let m=document.getElementById("menu");
+  m.style.display=m.style.display==="flex"?"none":"flex";
 }
 
-function searchService() {
-  let input = document.getElementById("searchInput").value.toLowerCase();
-  let cards = document.getElementsByClassName("card");
+function toggleSub(){
+  let s=document.getElementById("submenu");
+  s.style.display=s.style.display==="block"?"none":"block";
+}
 
-  for (let i = 0; i < cards.length; i++) {
-    let text = cards[i].innerText.toLowerCase();
-    cards[i].style.display = text.includes(input) ? "block" : "none";
+function goSection(id){
+  document.getElementById(id).scrollIntoView();
+}
+
+function notify(){
+  alert("🔔 Notification Enabled!");
+}
+
+function searchService(){
+  let input=document.getElementById("searchInput").value.toLowerCase();
+  let cards=document.getElementsByClassName("card");
+  for(let i=0;i<cards.length;i++){
+    cards[i].style.display=cards[i].innerText.toLowerCase().includes(input)?"block":"none";
   }
 }
