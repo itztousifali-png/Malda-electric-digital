9102de0956347958437159035292ac1fcbe3071d
itztousifali-png <itztousifali@gmail.com>
2025-12-26 21:05:16 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function notify() {
+  alert("🔔 Notification Enabled!\nYou will get updates soon.");
+}
+
+function searchService() {
+  let input = document.getElementById("searchInput").value.toLowerCase();
+  let cards = document.getElementsByClassName("card");
+
+  for (let i = 0; i < cards.length; i++) {
+    let text = cards[i].innerText.toLowerCase();
+    cards[i].style.display = text.includes(input) ? "block" : "none";
+  }
+}
